1f5d28b11b694f9ce2ff18628bb64d6b0235042b
JongwooMoon <luna@LUNAui-MacBook-Pro.local>
2017-09-17 15:23:16 +0900

    Ver 0.0.4. Add Naver Module.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 
 # rn-mtt-base
 React, JS My personal utils etc.
+
+## Requirements
+
+Not really considered. About Version 3+ requires RN 0.47 or higher
+
+## Installation
+
+```javascript
+npm install rn-mtt-base --save
+```
+or
+
+```javascript
+yarn add rn-mtt-base
+```
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+// Header Search Path in iOS
+//   $(SRCROOT)/../react-native/React
+//   $(SRCROOT)/../../React
+
+import { NativeModules, Platform } from 'react-native';
+
+const NaverLogin = NativeModules.RNNaverLogin;
 
 export function experi(arg) {
   console.log(arg);
   return `Hello world to arg : ${arg}`;
+}
+
+export function reachToIos(arg) {
+  // -(void)helloToCocoa:(NSString *)param {
+  NaverLogin.helloToCocoa(arg);
 }
